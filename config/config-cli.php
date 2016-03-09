62320846bea87e0272d7fc754fc2393b595efe36
barantaran <yourchev@gmail.com>
2016-03-09 13:44:58 +0300

    Inject view into cli di, add file log streamer

--- config/config-cli.php
+++ config/config-cli.php
@@ -1,5 +1,7 @@
 <?php
 return $config = [
+    'viewsDir'   => __DIR__ . '/../views/email/',
+    'logDir'   => __DIR__ . '/../log/',
     'driver'     => 'sendmail',
     'sendmail'   => '/usr/sbin/sendmail -bs',
     'from'       => [
