75e78c420f01ef517f626126e0fcf8776e62bfb8
etovolchanka <etovolchanka@gmail.com>
2016-03-31 15:03:52 +0300

    xhr! It works!

--- public/detail.js
+++ public/detail.js
@@ -1,18 +1,31 @@
 window.onload = function () {
-    var btn = document.getElementById('vote-btn');
-    btn.onclick = function () {
-        var params = {'id_competitive_work': this.getAttribute('data-id')};
-        var message = document.createElement('div');
-        $.post("/api/v1/vote", params, function (res) {
-            if (res['error']) {
+    document.getElementById('vote-btn').onclick = function () {
+        var params = {'id_competitive_work': this.getAttribute('data-id')},
+            message = document.createElement('div'),
+            xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"),
+            that = this;
+        xhr.open('POST', '/api/v1/vote');
+        xhr.send(JSON.stringify(params));
+        xhr.onreadystatechange = function () {
+            if (this.readyState != 4)
+                return;
+            else
+                var response = JSON.parse(xhr.responseText);
+            if (xhr.status != 200) {
                 message.className = 'vote error';
-                message.innerHTML = res['error']['label'];
-            } else if (res['success']) {
-                message.className = 'vote success';
-                message.innerHTML = res['success']['label'];
+                message.innerHTML = 'Ошибка! Попробуйте позднее.';
+            } else {
+                if (response['error']) {
+                    message.className = 'vote error';
+                    message.innerHTML = response['error']['label'];
+                } else if (response['success']) {
+                    message.className = 'vote success';
+                    message.innerHTML = response['success']['label'];
+                }
             }
-            btn.parentNode.removeChild(btn);
+            that.parentNode.removeChild(that);
             document.getElementById('buttons').appendChild(message);
-        });
+
+        }
     }
 };
